532462acef96655caaa45a88dc7db40c65555453
Filip Jeretina <jeretina.filip@gmail.com>
2026-01-19 13:53:30 +0100

    fix: major and minor number for devices in container edits.
    Use libc major/minor helpers instead of manual extraction.
    
    Signed-off-by: Filip Jeretina <jeretina.filip@gmail.com>

--- src/container_edits_unix.rs
+++ src/container_edits_unix.rs
@@ -27,23 +27,20 @@ impl fmt::Display for DeviceType {
 // deviceInfoFromPath takes the path to a device and returns its type, major and minor device numbers.
 // It was adapted from https://github.com/opencontainers/runc/blob/v1.1.9/libcontainer/devices/device_unix.go#L30-L69
 pub fn device_info_from_path<P: AsRef<Path>>(path: P) -> Result<(String, i64, i64)> {
-    let major = |dev: u64| -> i64 { (dev >> 8) as i64 & 0xff };
-    let minor = |dev: u64| -> i64 { dev as i64 & 0xff };
-
     let metadata = std::fs::metadata(path)?;
     let file_type = metadata.file_type();
 
     let (dev_type, major, minor) = if file_type.is_block_device() {
         (
             DeviceType::Block.to_string(),
-            major(metadata.rdev()),
-            minor(metadata.rdev()),
+            libc::major(metadata.rdev()),
+            libc::minor(metadata.rdev()),
         )
     } else if file_type.is_char_device() {
         (
             DeviceType::Char.to_string(),
-            major(metadata.rdev()),
-            minor(metadata.rdev()),
+            libc::major(metadata.rdev()),
+            libc::minor(metadata.rdev()),
         )
     } else if file_type.is_fifo() {
         (DeviceType::Fifo.to_string(), 0, 0)
@@ -51,7 +48,7 @@ pub fn device_info_from_path<P: AsRef<Path>>(path: P) -> Result<(String, i64, i6
         return Err(Error::new(ErrorKind::InvalidInput, "It's not a device node").into());
     };
 
-    Ok((dev_type, major, minor))
+    Ok((dev_type, major.into(), minor.into()))
 }
 
 #[cfg(test)]
